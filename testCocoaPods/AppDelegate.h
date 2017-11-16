221331d153002cd834d3dbdcc41d2b88344dc137
chenrunqian <121261655@qq.com>
2017-11-16 14:22:03 +0800

    Initial Commit

---
+++ testCocoaPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testCocoaPods
+//
+//  Created by sky on 2017/11/16.
+//  Copyright © 2017年 sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
